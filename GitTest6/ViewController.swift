d079c82d62b0269fbdd15d7a50237f5c0e4e67e7
Quentin Turner <q21turner@gmail.com>
2019-01-10 20:02:10 -0500

    Branch 1 Commit 2

--- GitTest6/ViewController.swift
+++ GitTest6/ViewController.swift
@@ -14,6 +14,7 @@ import AVFoundation
 class ViewController: UIViewController, ARSCNViewDelegate {
     
     var branch1 = "Branch1Commit1"
+    var branch1Commit2 = "BranchCommit2"
 
     @IBOutlet var sceneView: ARSCNView!
     
